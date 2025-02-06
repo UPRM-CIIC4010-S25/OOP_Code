e44369900f71022e76ca967e58dad89851f5239b
gretchypr <gretchen.bonilla@upr.edu>
2025-02-06 10:59:33 -0400

    Added the two Triangle class

---
+++ two_triangle_class.cpp
@@ -0,0 +1,17 @@
+class TwoTriangleClass
+{
+private:
+    /* data */
+public:
+    TwoTriangleClass(/* args */);
+    ~TwoTriangleClass
+();
+};
+
+TwoTriangleClass::TwoTriangleClass(/* args */)
+{
+}
+
+TwoTriangleClass::~TwoTriangleClass()
+{
+}
